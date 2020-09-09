42bbb43d91b16e342519703c1f44735bf5fa05de
guihRovetta <guilherme020213@hotmail.com>
2020-09-09 00:16:03 -0300

    💄 add global style and reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import type { AppProps } from 'next/app';
-import '../../styles/globals.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import GlobalStyles from 'styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
