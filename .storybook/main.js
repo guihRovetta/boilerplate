d388ba51a20a756f29f4fd2cfd0fb0eeca8b9559
guihRovetta <guilherme020213@hotmail.com>
2020-09-10 00:09:04 -0300

    🔧 update storybook configuration files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
+    '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-knobs',
   ],
 };
